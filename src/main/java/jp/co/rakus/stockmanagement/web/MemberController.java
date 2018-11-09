1d9b3889ea8706c7d4c96313ec25dae2128d72a2
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-11-09 14:12:14 +0900

    メンバー登録時に入力エラーを一度に表示するように編集

--- src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
@@ -56,28 +56,24 @@ public class MemberController {
 			BindingResult result,
 			Model model) {
 		
+		if(memberService.findMailAddress(form.getMailAddress()) != null) {
+			//model.addAttribute("duplicatedMessage", "そのメールアドレスは既に登録されています");
+			result.rejectValue("mailAddress", "MemberForm.global.duplicate", "そのメールアドレスは既に登録されています");
+		}
+		
+		if(form.getConfirmPassword().equals(form.getPassword())) {
+			//model.addAttribute("confirmMessage", "パスワードが一致しません");
+			result.rejectValue("password", "MemberForm.global.equal" ,"パスワードが一致しません");
+		}
+		
 		if(result.hasErrors()) {
 			return form();
 		}
 		
 		Member member = new Member();
-		
-		if(memberService.findMailAddress(form.getMailAddress()) != null) {
-			//model.addAttribute("duplicatedMessage", "そのメールアドレスは既に登録されています");
-			result.rejectValue("mailAddress", "MemberForm.global.duplicate", "そのメールアドレスは既に登録されています");
-			return form();
-		}
-		
-		if(form.getConfirmPassword().equals(form.getPassword())) {
-			BeanUtils.copyProperties(form, member);
-			memberService.save(member);
-		}else {
-			//model.addAttribute("confirmMessage", "パスワードが一致しません");
-			result.rejectValue("password", "MemberForm.global.equal" ,"パスワードが一致しません");
-			return form();
-		}
-		
+		memberService.save(member);
+		BeanUtils.copyProperties(form, member);
+	
 		return "redirect:/";
 	}
-	
 }
